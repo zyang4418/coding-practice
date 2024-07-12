06a1c36365d4023c026cb8714f9a5b0acf7cb7c6
zyang4418 <45694022+zyang4418@users.noreply.github.com>
2024-07-12 16:17:04 +0800

    程序清单 4.3

--- talkback.c
+++ talkback.c
@@ -1,5 +1,5 @@
 //程序清单 4.1 演示与用户交互
-//这个程序在 Visual Studio 2022 中不能正常运行
+//经过修改，这个程序在 Visual Studio 2022 中可以正常运行
 #include <stdio.h>
 #include <string.h> //提供 strlen() 函数的原型
 #define DENSITY 62.4 //人体密度（单位：磅/立方英尺）
@@ -10,9 +10,11 @@ int main(void)
 	char name[40]; //name 是一个可容纳 40 个字符的数组
 	
 	printf("Hi! What's your first name?\n");
-	scanf_s("%s", name); 
+	scanf_s("%s", name, sizeof(name)); 
+	//在此处添加了 sizeof(name) 后程序可正常运行
 	printf("%s, what's your weight in pounds?\n", name);
 	scanf_s("%f", &weight);
+	//在此处不能添加 sizeof(weight)，若添加则警告传递的参数过多
 	size = sizeof(name);
 	letters = strlen(name);
 	volume = weight / DENSITY;
