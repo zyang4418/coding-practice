76ef0b01babd294d8baf08a1213f62b6d9b18d6c
焯几何分布 <45694022+zyang4418@users.noreply.github.com>
2024-10-26 14:44:12 +0800

    程序清单 5.3

---
+++ golf.c
@@ -0,0 +1,12 @@
+﻿//程序清单 5.3 高尔夫锦标赛记分卡
+#include <stdio.h>
+int main(void)
+{
+	int jane, tarzan, cheeta;
+
+	cheeta = tarzan = jane = 68;
+	printf("                  cheeta   tarzan    jane\n");
+	printf("First round score %4d %8d %8d\n", cheeta, tarzan, jane);
+
+	return 0;
+}
