419a616185e02c148859f0a094098bd36d06c963
焯几何分布 <45694022+zyang4418@users.noreply.github.com>
2025-03-08 02:10:27 +0800

    delete some useless files

--- pintia/FileName.c
+++ pintia/FileName.c
@@ -1,40 +1,25 @@
 ﻿#include <stdio.h>
 
 int main() {
-    int year, month, day;
-    scanf("%d/%d/%d", &year, &month, &day);
-    int _31 = 31, _30 = 30, _29 = 29, _28 = 28;
+    double open, high, low, close;
+    scanf("%lf %lf %lf %lf", &open, &high, &low, &close);
 
-    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-        switch (month) {
-            case 1: printf("%d", day); break;
-            case 2: printf("%d", _31 + day); break;
-            case 3: printf("%d", _31 + _29 + day); break;
-            case 4: printf("%d", _31 * 2 + _29 + day); break;
-            case 5: printf("%d", _31 * 2 + _30 + _29 + day); break;
-            case 6: printf("%d", _31 * 3 + _30 + _29 + day); break;
-            case 7: printf("%d", _31 * 3 + _30 * 2 + _29 + day); break;
-            case 8: printf("%d", _31 * 4 + _30 * 2 + _29 + day); break;
-            case 9: printf("%d", _31 * 4 + _30 * 2 + _29 + day); break;
-            case 10: printf("%d", _31 * 4 + _30 * 3 + _29 + day); break;
-            case 11: printf("%d", _31 * 5 + _30 * 3 + _29 + day); break;
-            case 12: printf("%d", _31 * 5 + _30 * 4 + _29 + day); break;
-        }
-    } else {
-        switch (month) {
-            case 1: printf("%d", day); break;
-            case 2: printf("%d", _31 + day); break;
-            case 3: printf("%d", _31 + _28 + day); break;
-            case 4: printf("%d", _31 * 2 + _28 + day); break;
-            case 5: printf("%d", _31 * 2 + _30 + _28 + day); break;
-            case 6: printf("%d", _31 * 3 + _30 + _28 + day); break;
-            case 7: printf("%d", _31 * 3 + _30 * 2 + _28 + day); break;
-            case 8: printf("%d", _31 * 4 + _30 * 2 + _28 + day); break;
-            case 9: printf("%d", _31 * 4 + _30 * 2 + _28 + day); break;
-            case 10: printf("%d", _31 * 4 + _30 * 3 + _28 + day); break;
-            case 11: printf("%d", _31 * 5 + _30 * 3 + _28 + day); break;
-            case 12: printf("%d", _31 * 5 + _30 * 4 + _28 + day); break;
-        }
+    if (close < open) {
+        printf("BW-Solid");
+    } else if (close > open) {
+        printf("R-Hollow");
+    } else if (close == open) {
+        printf("R-Cross");
     }
+
+    if (low < open && low < close && high > open && high > close) {
+        printf(" with Lower Shadow and Upper Shadow");
+    }
+    else if (low < open && low < close) {
+        printf(" with Lower Shadow");
+    } else if (high > open && high > close) {
+        printf(" with Upper Shadow");
+    }
+
     return 0;
 }
