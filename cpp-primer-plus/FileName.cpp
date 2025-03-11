fbdd73a812d2e61da2261c39c7c0a54cea1418a5
焯几何分布 <45694022+zyang4418@users.noreply.github.com>
2025-03-11 23:22:33 +0800

    cpp

--- cpp-primer-plus/FileName.cpp
+++ cpp-primer-plus/FileName.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+using namespace std;
+
+void c(int &a) {
+    a += 1;
+}
 
 int main() {
-    using namespace std;
-    cout << "Hello World!\n";
+    int a = 4;
+    c(a);
+    cout << a << endl;
 
     return 0;
 }
