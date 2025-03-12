b608d8928526640bcf8423dab811a5ffe813eb18
焯几何分布 <45694022+zyang4418@users.noreply.github.com>
2025-03-12 17:02:48 +0800

    cpp-primer-plus updated

--- pintia/FileName.c
+++ pintia/FileName.c
@@ -1,16 +1,23 @@
-﻿#include <stdio.h>
+﻿#include<stdio.h>
 
 int main() {
-    double len, high, x, y, z;
-    scanf("%lf %lf %lf %lf %lf", &len, &high, &x, &y, &z);
 
-    double abef, ecgh, dfhg;
-    abef = (x + y) * high / 2;
-    printf("Area of ABEF is %.3lf\n", abef);
-    ecgh = (len - x) * high / 4 + ( (y - x) / 2 + len - y) * z / 2;
-    printf("Area of ECGH is %.3lf\n", ecgh);
-    dfhg = (high - z) * ( (y - x) / 2 + len - y) / 2 + (len - y) * high / 4;
-    printf("Area of DFHG is %.3lf\n", dfhg);
+	int a, b;
 
-    return 0;
+	for(a=1,b=1; a<=10; a++) {
+
+		if(b>=10) break;
+
+		if(b%3==1) {
+
+			b+=3;
+
+			continue;
+
+		}
+
+	}
+
+	printf("%d",a);
+
 }
---
+++ pintia/FileName.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <iomanip>
+
+int main() {
+    using namespace std;
+    int x; // 题目给定的待换零钱数额x，[8,100]
+    cin >> x;
+
+    return 0;
+}
