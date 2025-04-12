3928f7c4f823b6123592a96f538fac0b899bac2b
焯几何分布 <45694022+zyang4418@users.noreply.github.com>
2025-04-12 23:19:11 +0800

    4/12/25

--- pintia/FileName.cpp
+++ pintia/FileName.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-
-
+    string s;
+    getline(cin, s);
+    int i = 0;
+    while (i < s.length()) {
+        if (s.at(i) != '6') {
+            cout << s.at(i);
+            i++;
+        } else {
+            int count = 0;
+            while (i < s.length() && s.at(i) == '6') {
+                count++;
+                i++;
+            }
+            if (count > 9) {
+                cout << "27";
+            } else if (count > 3) {
+                cout << "9";
+            } else {
+                for (int j = 0; j < count; j++) {
+                    cout << '6';
+                }
+            }
+        }
+    }
     return 0;
 }
---
+++ python/test.py
@@ -0,0 +1,8 @@
+pm = int(input())
+
+if pm < 35:
+    print("空气优质，快去户外运动!")
+elif pm < 75:
+    print("空气良好，适合户外活动!")
+else:
+    print("空气污染，请小心！")
