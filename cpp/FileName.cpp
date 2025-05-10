d2ee247703f8d801e1583f4b92b49d81fabca229
焯几何分布 <45694022+zyang4418@users.noreply.github.com>
2025-05-11 02:58:36 +0800

    5/11/25

--- cpp/FileName.cpp
+++ cpp/FileName.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <iomanip>
 #include <string>
 
@@ -10,32 +9,6 @@ struct book {
 };
 
 int main() {
-    int n; // n is a number that > 0 and < 10
-    cin >> n;
 
-    string* books = new string[n];
-    double* prices = new double[n];
-
-    for (int i = 0; i < n; i++) {
-        cin.ignore(); // 忽略换行符
-        getline(cin, books[i]);
-        cin >> prices[i];
-    }
-
-    int maxIndex = 0, minIndex = 0;
-    for (int i = 1; i < n; i++) {
-        if (prices[i] > prices[maxIndex]) {
-            maxIndex = i;
-        }
-        if (prices[i] < prices[minIndex]) {
-            minIndex = i;
-        }
-    }
-
-    cout << fixed << setprecision(2);
-    cout << prices[maxIndex] << ", " << books[maxIndex] << endl;
-    cout << prices[minIndex] << ", " << books[minIndex] << endl;
-
-    delete[] books;
-    delete[] prices;
+    return 0;
 }
